1d22a1153fa44806854392df7cb3e3cab532ecf9
Leonid Caminschi <leocer.leonid@gmail.com>
2023-09-28 17:15:27 +0300

    Added exportation of absolute links to txt document

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from in_class import webcrawl
 
 # Replace with the URL of the web page you want to scrape
-url1 = "https://999.md/ro/list/computers-and-office-equipment/laptops?page="
+url1 = "https://999.md/ro/list/computers-and-office-equipment/laptops"
 
-print(webcrawl(url1))
+list_of_urls = webcrawl(url1)
+print(list_of_urls)
+
+# with open("urls.txt", 'w') as file:
+#     for url in list_of_urls:
+#         file.write(url + '\n')
