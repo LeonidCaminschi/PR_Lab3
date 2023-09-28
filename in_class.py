40598027968b0ced33752e715bf3286513695dda
Leonid Caminschi <leocer.leonid@gmail.com>
2023-09-28 17:03:29 +0300

    Added in class exercise

---
+++ in_class.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def webcrawl(url, page=1):
+    list_of_urls = set()
+
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url + str(page))
+        print(url + str(page))
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the HTML content of the page
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Find all anchor (link) tags in the HTML
+            links = soup.find_all('a')
+
+            # Extract and print the href attribute of each link
+            regex = re.compile("/ro/[0-9]")
+            for link in links:
+                href = link.get('href')
+                if href is not None and regex.match(href):
+                    list_of_urls.add(href)
+
+            if len(list_of_urls) == 0:
+                return list_of_urls
+
+            list_of_urls.update(webcrawl(url, page + 1))
+
+            return list_of_urls
+
+        else:
+            print(f"Failed to retrieve the web page. Status code: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during the request: {e}")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
---
+++ main.py
@@ -0,0 +1,6 @@
+from in_class import webcrawl
+
+# Replace with the URL of the web page you want to scrape
+url1 = "https://999.md/ro/list/computers-and-office-equipment/laptops?page="
+
+print(webcrawl(url1))
